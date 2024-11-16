4a4457b421158f94810f5b7717bf5943deebabdf
Eduardo Kraus <kraus@eduardokraus.com>
2024-11-16 17:59:41 -0300

    V 1.2.8

--- version.php
+++ version.php
@@ -24,8 +24,8 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version = 2024111100;
-$plugin->release = "1.2.7";
+$plugin->version = 2024111600;
+$plugin->release = "1.2.8";
 $plugin->requires = 2011120500;
 $plugin->component = 'local_kopere_mobile';
 $plugin->dependencies = ["local_kopere_dashboard" => 2024101500];
