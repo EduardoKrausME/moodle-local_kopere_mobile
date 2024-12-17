08894410f01be06ecb39bb3255a83ca4cf2a0a8e
Eduardo Kraus <kraus@eduardokraus.com>
2024-12-17 12:21:33 -0300

    Melhorias

--- lang/en/local_kopere_mobile.php
+++ lang/en/local_kopere_mobile.php
@@ -22,13 +22,30 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-$string['modulename'] = 'Kopere Mobile';
-$string['pluginname'] = 'Kopere Mobile';
-$string['facial:view'] = 'View Kopere Mobile';
-$string['facial:manage'] = 'Manager Kopere Mobile';
-$string['settings'] = 'Setting Kopere Mobile';
 
-$string['download_title'] = 'Download mobile device';
+
+$string['androidappfile'] = 'File to download the Android app';
+$string['androidappfile_desc'] = 'Leave this setting blank unless you have an Android app hosted on your server.';
+$string['androidappid'] = 'The unique identifier of the Android app';
+$string['androidappid_desc'] = 'This setting can be left as default unless you have a custom Android application.';
+$string['app_title'] = 'APP Data in stores';
+$string['config_deleted'] = 'User deleted!';
+$string['config_lgpd_reason'] = 'Motivo da exclusão';
+$string['config_not_confirmed'] = 'User not confirmed!';
+$string['config_not_manual'] = 'User must have Manual authentication!';
+$string['config_not_user'] = 'User needs to be created';
+$string['config_suspended'] = 'User suspended!';
+$string['config_test_correct'] = 'Valor está incorreto para funcionamento do APP';
+$string['config_test_title'] = 'Status das configurações necessárias para o funcionamento do APP';
+$string['customizationapp'] = 'APP Customization';
+$string['customizationappcss'] = 'APP Customization CSS';
+$string['customizationappcss_desc'] = 'This file contains the custom CSS that will be applied to the app, allowing visual adjustments according to the specific needs of the project.';
+$string['customizationapphome'] = 'Course list on Home';
+$string['customizationapphome_background'] = 'Background image';
+$string['customizationapphome_default'] = 'Image on the side';
+$string['customizationapphome_desc'] = 'This option allows you to customize the display of courses on the application\'s home page. You can choose from different layouts and display styles to highlight the courses according to your preferences and needs.';
+$string['customizationapptopo'] = 'Top image on the APP home screen';
+$string['customizationapptopo_desc'] = 'This field allows you to upload an image that will be displayed as a header on the app\'s home page. Dimensions: 1080 x 441px';
 $string['download_info'] = '<p>With the Moodle app, you can learn from anywhere, anytime, with the following app features:</p>
     <ul>
         <li>Easily access course content - browse your course content with ease, even when you are offline.</li>
@@ -38,55 +55,36 @@ $string['download_info'] = '<p>With the Moodle app, you can learn from anywhere,
         <li>Complete activities anywhere, anytime, play SCORM packages, and more, both online and offline.</li>
         <li>... and much more!</li>
     </ul>';
-
-$string['config_not_manual'] = 'User must have Manual authentication!';
-$string['config_not_confirmed'] = 'User not confirmed!';
-$string['config_deleted'] = 'User deleted!';
-$string['config_suspended'] = 'User suspended!';
-$string['config_not_user'] = 'User needs to be created';
-
-$string['setting_title'] = 'Kopere Mobile Settings';
-
-$string['status_app'] = 'APP Status';
-$string['customizationapp'] = 'APP Customization';
-$string['logologin'] = 'PNG/SVG logo for login screen';
-$string['logologin_desc'] = 'Upload the logo in PNG or SVG format to be displayed on the login screen. The logo should be clear and representative of your institution or company\'s visual identity, ensuring a professional and consistent login experience for users.';
-$string['customizationapptopo'] = 'Top image on the APP home screen';
-$string['customizationapptopo_desc'] = 'This field allows you to upload an image that will be displayed as a header on the app\'s home page. Dimensions: 1080 x 441px';
-$string['customizationapphome'] = 'Course list on Home';
-$string['customizationapphome_desc'] = 'This option allows you to customize the display of courses on the application\'s home page. You can choose from different layouts and display styles to highlight the courses according to your preferences and needs.';
-$string['customizationapphome_default'] = 'Image on the side';
-$string['customizationapphome_background'] = 'Background image';
-$string['customizationappcss'] = 'APP Customization CSS';
-$string['customizationappcss_desc'] = 'This file contains the custom CSS that will be applied to the app, allowing visual adjustments according to the specific needs of the project.';
+$string['download_title'] = 'Download mobile device';
+$string['facial:manage'] = 'Manager Kopere Mobile';
+$string['facial:view'] = 'View Kopere Mobile';
 $string['htmllogin'] = 'Extra Login HTML';
 $string['htmllogin_desc'] = 'This setting allows you to add custom HTML code to the login page. It can be used to include additional contact and support information, such as emails, phone numbers, or useful links for users. The HTML inserted here will be displayed directly on the login interface.';
-
-$string['app_title'] = 'APP Data in stores';
 $string['iosappid'] = 'IOS app numeric ID';
 $string['iosappid_desc'] = 'This setting can be left as the default unless you have a custom iOS application.';
-$string['androidappid'] = 'The unique identifier of the Android app';
-$string['androidappid_desc'] = 'This setting can be left as default unless you have a custom Android application.';
-$string['androidappfile'] = 'File to download the Android app';
-$string['androidappfile_desc'] = 'Leave this setting blank unless you have an Android app hosted on your server.';
-
-$string['lgpd_title'] = 'Information about the General Data Protection Law';
 $string['lgpd_email'] = 'Data deletion email';
 $string['lgpd_email_desc'] = 'Enter the email address to direct data deletion requests.';
-$string['lgpd_text'] = 'Data deletion policy';
-$string['lgpd_text_desc'] = 'Describe the Data Deletion Policy in accordance with the General Data Protection Law in Moodle';
-$string['lgpd_text_msgdefault'] = '<p>In compliance with the General Data Protection Law, this page aims to provide detailed information on how we handle and protect your personal data within the Moodle virtual environment. The General Data Protection Law\'s primary purpose is to ensure the privacy and security of personal information, promoting transparency and accountability in data processing.</p>
-<p>You have the right to request the deletion of your personal data stored in our system. However, it is important to note that, according to current legislation and regulations from the Ministry of Education (MEC), some data is mandatory for the maintenance of academic and administrative records.</p>
-<p>We appreciate everyone\'s understanding in ensuring compliance with the General Data Protection Law and other applicable regulations. We are available to clarify any additional questions or provide specific information about the processing of your personal data.</p>
-<h2>Data Deletion Request Form:</h2>
-<p>To request the deletion of your data, please fill out the form below. Upon receiving your request, we will conduct an analysis and respond as quickly as possible to ensure legal compliance.</p>';
 $string['lgpd_okok'] = 'Submission confirmation message';
 $string['lgpd_okok_desc'] = 'Message after the data has been sent to the responsible party';
 $string['lgpd_okok_msgdefault'] = '<p>Thank you for submitting your data deletion request. We are committed to ensuring the security and privacy of our users\' information.</p>
 <p>Your request has been successfully received and will be processed by our data protection team. We emphasize that, according to current legislation, only non-mandatory data will be deleted, preserving essential information required by legal authorities.</p>
 <p>If additional information is needed or if you have any questions, our team will contact you within the timeframe stipulated by law. Please keep your contact details updated to facilitate communication.</p>
 <p>Thank you for your understanding and trust in our platform. We are here to ensure a safe and transparent experience.</p>';
+$string['lgpd_text'] = 'Data deletion policy';
+$string['lgpd_text_desc'] = 'Describe the Data Deletion Policy in accordance with the LGPD in Moodle';
+$string['lgpd_text_msgdefault'] = '<p>In compliance with the General Data Protection Law (LGPD), this page aims to provide detailed information on how we handle and protect your personal data within the Moodle virtual environment. The LGPD\'s primary purpose is to ensure the privacy and security of personal information, promoting transparency and accountability in data processing.</p>
+<p>You have the right to request the deletion of your personal data stored in our system. However, it is important to note that, according to current legislation and regulations from the Ministry of Education (MEC), some data is mandatory for the maintenance of academic and administrative records.</p>
+<p>We appreciate everyone\'s understanding in ensuring compliance with the LGPD and other applicable regulations. We are available to clarify any additional questions or provide specific information about the processing of your personal data.</p>
+<h2>Data Deletion Request Form:</h2>
+<p>To request the deletion of your data, please fill out the form below. Upon receiving your request, we will conduct an analysis and respond as quickly as possible to ensure legal compliance.</p>';
+$string['lgpd_title'] = 'Information about the LGPD';
+$string['logologin'] = 'PNG/SVG logo for login screen';
+$string['logologin_desc'] = 'Upload the logo in PNG or SVG format to be displayed on the login screen. The logo should be clear and representative of your institution or company\'s visual identity, ensuring a professional and consistent login experience for users.';
+$string['modulename'] = 'Kopere Mobile';
+$string['pluginname'] = 'Kopere Mobile';
+$string['privacy:metadata'] = 'The Plugin custom field plugin does not store any personal data';
+$string['setting_title'] = 'Kopere Mobile Settings';
+$string['settings'] = 'Setting Kopere Mobile';
+$string['status_app'] = 'APP Status';
 
-$string['config_test_title']='Status das configurações necessárias para o funcionamento do APP';
-$string['config_test_correct']='Valor está incorreto para funcionamento do APP';
-$string['config_lgpd_reason']='Motivo da exclusão';
+
