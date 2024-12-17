793749a08df0790a51aa20a2b689921af69dc262
Eduardo Kraus <kraus@eduardokraus.com>
2024-12-17 12:09:17 -0300

    General Data Protection Law

--- lang/en/local_kopere_mobile.php
+++ lang/en/local_kopere_mobile.php
@@ -70,14 +70,14 @@ $string['androidappid_desc'] = 'This setting can be left as default unless you h
 $string['androidappfile'] = 'File to download the Android app';
 $string['androidappfile_desc'] = 'Leave this setting blank unless you have an Android app hosted on your server.';
 
-$string['lgpd_title'] = 'Information about the LGPD';
+$string['lgpd_title'] = 'Information about the General Data Protection Law';
 $string['lgpd_email'] = 'Data deletion email';
 $string['lgpd_email_desc'] = 'Enter the email address to direct data deletion requests.';
 $string['lgpd_text'] = 'Data deletion policy';
-$string['lgpd_text_desc'] = 'Describe the Data Deletion Policy in accordance with the LGPD in Moodle';
-$string['lgpd_text_msgdefault'] = '<p>In compliance with the General Data Protection Law (LGPD), this page aims to provide detailed information on how we handle and protect your personal data within the Moodle virtual environment. The LGPD\'s primary purpose is to ensure the privacy and security of personal information, promoting transparency and accountability in data processing.</p>
+$string['lgpd_text_desc'] = 'Describe the Data Deletion Policy in accordance with the General Data Protection Law in Moodle';
+$string['lgpd_text_msgdefault'] = '<p>In compliance with the General Data Protection Law, this page aims to provide detailed information on how we handle and protect your personal data within the Moodle virtual environment. The General Data Protection Law\'s primary purpose is to ensure the privacy and security of personal information, promoting transparency and accountability in data processing.</p>
 <p>You have the right to request the deletion of your personal data stored in our system. However, it is important to note that, according to current legislation and regulations from the Ministry of Education (MEC), some data is mandatory for the maintenance of academic and administrative records.</p>
-<p>We appreciate everyone\'s understanding in ensuring compliance with the LGPD and other applicable regulations. We are available to clarify any additional questions or provide specific information about the processing of your personal data.</p>
+<p>We appreciate everyone\'s understanding in ensuring compliance with the General Data Protection Law and other applicable regulations. We are available to clarify any additional questions or provide specific information about the processing of your personal data.</p>
 <h2>Data Deletion Request Form:</h2>
 <p>To request the deletion of your data, please fill out the form below. Upon receiving your request, we will conduct an analysis and respond as quickly as possible to ensure legal compliance.</p>';
 $string['lgpd_okok'] = 'Submission confirmation message';
