b02f2897e49f418742382b323515835bcf285f53
Eduardo Kraus <kraus@eduardokraus.com>
2024-12-17 12:18:13 -0300

    SESSION to USER

--- lib.php
+++ lib.php
@@ -30,25 +30,21 @@
  * @throws dml_exception
  */
 function local_kopere_mobile_before_standard_html_head() {
-    global $SESSION, $PAGE;
+    global $USER, $PAGE;
 
     ob_start();
 
     $PAGE->requires->js_call_amd('local_kopere_mobile/picture', 'move');
 
-    if (isset($SESSION->kopere_mobile_preserve_page) && $SESSION->kopere_mobile_preserve_page) {
-        $preservepage = $SESSION->kopere_mobile_preserve_page;
-
-        if (strpos($_SERVER['REQUEST_URI'], $preservepage) !== false) {
-            // Não faz nada aqui.
-        } else if (isset($SESSION->kopere_mobile_redirect_page[5])) {
-            header("Location: {$SESSION->kopere_mobile_redirect_page}");
+    if (isset($USER->kopere_mobile_preserve_page) && $USER->kopere_mobile_preserve_page) {
+        if (isset($USER->kopere_mobile_redirect_page[5])) {
+            header("Location: {$USER->kopere_mobile_redirect_page}");
             header("kopere_mobile-status: event_observers::process_event");
             die();
         }
     }
 
-    if (isset($SESSION->kopere_mobile_mobile) && $SESSION->kopere_mobile_mobile) {
+    if (isset($USER->kopere_mobile_mobile) && $USER->kopere_mobile_mobile) {
         $PAGE->set_pagelayout('embedded');
         $return = "
             <meta http-equiv=\"Content-Security-Policy\"
@@ -76,42 +72,15 @@ function local_kopere_mobile_before_standard_html_head() {
  *
  */
 function local_kopere_mobile_before_http_headers() {
-    global $SESSION, $PAGE;
+    global $USER, $PAGE;
 
-    $iskoperemobilemobile = isset($SESSION->kopere_mobile_mobile) && $SESSION->kopere_mobile_mobile;
+    $iskoperemobilemobile = isset($USER->kopere_mobile_mobile) && $USER->kopere_mobile_mobile;
     if ($iskoperemobilemobile || optional_param("kopere_mobile_mobile", false, PARAM_INT)) {
 
         $PAGE->set_pagelayout('embedded');
         $PAGE->requires->css("/local/kopere_bi/assets/embedded.css");
         if ($PAGE->theme->name == "edooc") {
             $PAGE->requires->css("/local/kopere_bi/assets/edooc-embedded.css");
-        }
-    }
-}
-
-/**
- * local_kopere_mobile_before_footer function
- *
- * @throws coding_exception
- */
-function local_kopere_mobile_before_footer() {
-    global $CFG;
-
-    $openedin = optional_param("openedin", false, PARAM_TEXT);
-    if ($openedin == 'AppMoodleMobileV2' || strpos($_SERVER['HTTP_USER_AGENT'], "AppMoodleMobileV2")) {
-
-        if (strpos($_SERVER['REQUEST_URI'], 'mod/scorm/player.php') > 0 ||
-            strpos($_SERVER['REQUEST_URI'], 'local/kopere_mobile/scorm/player.php') > 0) {
-
-            $html = ob_get_contents();
-            ob_clean();
-
-            $html = str_replace("www.googletagmanager.com", "", $html);
-            $html = str_replace("vlibras.gov.br", "", $html);
-
-            echo $html;
-            echo "\n\n\n<script></script>\n";
-            echo "<link rel='stylesheet' href='{$CFG->wwwroot}/local/kopere_mobile/scorm/scorm.css'/>";
         }
     }
 }
@@ -135,8 +104,6 @@ function local_kopere_mobile_before_footer() {
  * @throws Exception
  */
 function local_kopere_mobile_pluginfile($course, $cm, $context, $filearea, $args, $forcedownload, array $options = []) {
-    global $CFG, $DB;
-
     if ($filearea == 'androidappfile') {
         return local_kopere_mobile_pluginfile_sendfile($context, $filearea, $args, $options);
     } else if ($filearea == 'customizationapptopo') {
@@ -144,97 +111,6 @@ function local_kopere_mobile_pluginfile($course, $cm, $context, $filearea, $args
     } else if ($filearea == 'logologin') {
         return local_kopere_mobile_pluginfile_sendfile($context, $filearea, $args, $options);
     }
-
-    if ($context->contextlevel != CONTEXT_MODULE) {
-        return false;
-    }
-
-    require_once(__DIR__ . "/scorm/lib.php");
-
-    require_login($course, true, $cm);
-
-    $canmanageactivity = has_capability('moodle/course:manageactivities', $context);
-    $lifetime = null;
-
-    // Check SCORM availability.
-    if (!$canmanageactivity) {
-        require_once($CFG->dirroot . '/mod/scorm/locallib.php');
-
-        $scorm = $DB->get_record('scorm', ['id' => $cm->instance], 'id, timeopen, timeclose', MUST_EXIST);
-        list($available, $warnings) = scorm_get_availability_status($scorm);
-        if (!$available) {
-            return false;
-        }
-    }
-
-    if ($filearea === 'content') {
-        $revision = (int)array_shift($args); // Prevents caching problems - ignored here.
-        $relativepath = implode('/', $args);
-        $fullpath = "/$context->id/mod_scorm/content/0/$relativepath";
-        $options['immutable'] = true; // Add immutable option, $relativepath changes on file update.
-
-    } else if ($filearea === 'package') {
-        // Check if the global setting for disabling package downloads is enabled.
-        $protectpackagedownloads = get_config('scorm', 'protectpackagedownloads');
-        if ($protectpackagedownloads && !$canmanageactivity) {
-            return false;
-        }
-        $revision = (int)array_shift($args); // Prevents caching problems - ignored here.
-        $relativepath = implode('/', $args);
-        $fullpath = "/$context->id/mod_scorm/package/0/$relativepath";
-        $lifetime = 0; // No caching here.
-
-    } else if ($filearea === 'imsmanifest') { // This isn't a real filearea, it's a url parameter for this type of package.
-        $revision = (int)array_shift($args); // Prevents caching problems - ignored here.
-        $relativepath = implode('/', $args);
-
-        // Get imsmanifest file.
-        $fs = get_file_storage();
-        $files = $fs->get_area_files($context->id, 'mod_scorm', 'package', 0, '', false);
-        $file = reset($files);
-
-        // Check that the package file is an imsmanifest.xml file - if not then this method is not allowed.
-        $packagefilename = $file->get_filename();
-        if (strtolower($packagefilename) !== 'imsmanifest.xml') {
-            return false;
-        }
-
-        $file->send_relative_file($relativepath);
-    } else {
-        return false;
-    }
-
-    $fs = get_file_storage();
-    if (!$file = $fs->get_file_by_hash(sha1($fullpath)) || $file->is_directory()) {
-        if ($filearea === 'content') { // Return file not found straight away to improve performance.
-            send_header_404();
-            die;
-        }
-        return false;
-    }
-
-    // Allow SVG files to be loaded within SCORM content, instead of forcing download.
-    $options['dontforcesvgdownload'] = true;
-
-    header("kraus-filearea: " . $filearea);
-    $pasta = implode("/", $args);
-    if (strpos($pasta, ".html")) {
-        localscorm_send_stored_file($file, $lifetime, $options);
-
-        global $plugins;
-        require_once("{$CFG->dirroot}/lib/jquery/plugins.php");
-
-        echo "
-            <script src='{$CFG->wwwroot}/lib/jquery/{$plugins['jquery']['files'][0]}'></script>
-            <script src='{$CFG->wwwroot}/local/kopere_mobile/scorm/pdf/scorm-link.js'></script>
-            <script src='{$CFG->wwwroot}/local/kopere_mobile/scorm/pdf/modal.js'></script>
-            <link  href='{$CFG->wwwroot}/local/kopere_mobile/scorm/pdf/modal.css' rel='stylesheet'/>";
-        die();
-    }
-
-    // Finally send the file.
-    send_stored_file($file, $lifetime, 0, false, $options);
-    return true;
 }
 
 /**
